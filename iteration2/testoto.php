bdadbb6395b5994ccbc96dffb6bb3ea8ee7b1889
AhmedHamma <73638564+AhmedHamma@users.noreply.github.com>
2020-12-04 09:35:45 +0100

    fin iteration 4

--- iteration2/testoto.php
+++ iteration2/testoto.php
@@ -1,4 +1,33 @@
 <?php
-include("article.php");
-displayItem(["nom"],["prix"],["image"]);
+function displayItem1(){
+    echo $nom;
+    echo $prix;
+    echo $photo;
+}
+
+}
+function displayItem2(){
+
+    $nom = "voiture noire";
+    $prix = " 1500euro";
+    $photo = "<img src=voiturenoir.jpg alt=''/>";;
+    $alt = "voiture noire";
+    $item=[$nom,$prix,$photo];
+    for($i=0;$i<count ($item); $i++){
+        echo $item[$i] . '<br/>';
+    }
+
+}
+function displayItem3(){
+
+    $nom = "voiture blanche";
+    $prix = " 1500euro";
+    $photo = "<img src=voitureblanche.jpg alt=''/>";
+    $alt = "voiture blanche";
+    $item=[$nom,$prix,$photo];
+    for($i=0;$i<count ($item); $i++){
+        echo $item[$i] . '<br/>';
+    }
+
+}
 ?>
